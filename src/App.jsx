7024367ab8f54c5886b82e0abfa084381e857e46
Abdulah2203 <professorshaha99@gmailcom>
2024-10-17 16:19:23 +0500

    Add nevbar and footer in component folder and implement in app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
 import Home from './pages/Home'
+import Nevbar from './Component/Nevbar.jsx'
+import Footer from './Component/Footer.jsx' 
 
 function App() {
 
   return (
     <>
+   <Nevbar/> 
+
       <Home />
+      <Footer/>
+
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Nevbar from './Component/Nevbar.jsx'
+import Footer from './Component/Footer.jsx' 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+   <Nevbar/> 
+    
+     <App />
+   <Footer/>
+   </StrictMode>,
 )
